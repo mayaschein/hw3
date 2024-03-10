3bd2ff2a3aadb5a8bf2770554866e7e357bb41f5
Moomin <maya.schein@campus.technion.ac.il>
2024-03-10 18:38:28 +0200

    updated Queue.h

--- Queue.h
+++ Queue.h
@@ -6,45 +6,51 @@
  * A queue is a data structure that follows the First In, First Out (FIFO) principle.
 */
 
-template<class ElementType> 
+template<class T> 
+class QueueNode{
+    T data;
+    QueueNode* next;
+};
+
+template<class T> 
  class Queue{
     public:
 
     /*
     * EmptyQueue:
-    * This exception is raised for attempts to perform an operation which is unsupported on an empty queue.
+    * Exception in case that a function which isn't supported on empty queues gets an empty queue.
     */
     class EmptyQueue{};
 
     /*
      * pushBack:
      * The function adds a new element to the end of the queue.
-     * @param &element Reference to the new element.
+     * @param &element Reference to the new element. Type must be as the other elements in the queue.
      * @exception If a memory allocation fails, the function will throw bad_alloc. The queue will remain unchanged.
     */
-    void pushBack(const ElementType &element);
+    void pushBack(const T &element);
 
     /*
     * front:
     * Gets the first element in the queue.
-    * @return a reference to the first element in the queue.
+    * @return reference to the first element in the queue.
     * @exception If the queue is empty, it will abort and throw EmptyQueue.
     */
-    ElementType &front();
+    T &front();
 
     /*
     * front(const):
-    * Gets the first element in the queue.
-    * @return a const reference to the first element in the queue.
+    * Gets the first element in the queue in read-only format.
+    * @return const reference to the first element in the queue.
     * @exception If the queue is empty, it will abort and throw EmptyQueue.
     */
-    const ElementType &front();
+    const T &front();
 
    /*
    * popFront:
    * The function removes the first element in the queue.
    * Notice that it doesn't return its value.
-   * @exception If the queue is empty, nothing is done???????????????????.
+   * @exception If the queue is empty, it will abort and throw EmptyQueue.
    */
     void popFront();
 
@@ -56,12 +62,15 @@ template<class ElementType>
     int size();
 
     /*
-    * non-const iterator. 
+    * non-const iterator. Only supports non-const queues.
+    * Look at the class definition below for more info.
     * @warning The iterator cannot be used after any change to the queue has been made.
     */
     class Iterator;
+
     /*
     * const iterator. 
+    * Look at the class definition below for more info.
     */
     class ConstIterator;
 
@@ -84,21 +93,37 @@ template<class ElementType>
 
     private:
     
+    //m_back->.....->m_front
+    QueueNode m_front;
+    QueueNode m_back;
+
  };
 
-/*insert note!!!!!!!!!!!!!!!!!!!!*/
-template<class ElementType>
-class Queue<ElementType>::Iterator{
+
+/*
+* Iterator for Queue:
+*/
+
+template<class T>
+class Queue<T>::Iterator{
+    /*
+    * InvalidOperation:
+    * Exception in case that an operation on an iterator is invalid.
+    */
+    class InvalidOperation{};
+
     /*
     * *( ) operator:
-    * Gets a read-only version of the element that an iterator points to.
+    * Gets a read-only version of the element that the iterator points to.
     * @return const reference to the element.
+    * @exception If the iterator points next to the last member, InvalidOperation is thrown.
     */
-    const ElementType& operator*() const;
+    const T& operator*() const;
 
     /*
     * ++( ) operator:
     * Moves an iterator to the next element.
+    * @exception If the iterator points next to the last member, InvalidOperation is thrown.
     */
     Iterator& operator++();
     
@@ -113,7 +138,7 @@ class Queue<ElementType>::Iterator{
     
 
     /*
-    * c'tor and assignment operator of the iterator is default.
+    * c'tor and assignment operator of the iterator is default????????/.
     */
     Iterator(const Iterator&) = default;
     Iterator& operator=(const Iterator&) = default;
@@ -121,19 +146,27 @@ class Queue<ElementType>::Iterator{
 
 };
 
-/*insert note!!!!!!!!!!!!!!!!!!!!*/
-template<class ElementType>
-class Queue<ElementType>::ConstIterator{
+/*const Iterator for Queue*/
+template<class T>
+class Queue<T>::ConstIterator{
     /*
-    * *( ) operator:
-    * Gets a read-only version of the element that an iterator points to.
-    * @return const reference to the element.
+    * InvalidOperation:
+    * Exception in case that an operation on an iterator is invalid.
     */
-    const ElementType& operator*() const;
+    class InvalidOperation{};
+
+   /*
+    * *( ) operator:
+    * Gets a read-only version of the element that the iterator points to.
+    * @return const reference to the element.
+    * @exception If the iterator points next to the last member, InvalidOperation is thrown.
+    */
+    const T& operator*() const;
 
     /*
     * ++( ) operator:
     * Moves an iterator to the next element.
+    * @exception If the iterator points next to the last member, InvalidOperation is thrown.
     */
     constIterator& operator++();
 
@@ -157,28 +190,28 @@ class Queue<ElementType>::ConstIterator{
 * filter:
 * The function takes a queue and a predict, applies it to each element in the queue, and creates a new queue
 * consisting only of elements for which the predict returns true.
-* @param queue - A queue of type ElementType.
+* @param queue - A queue of type T.
 * @param predict - A pointer to a function or a function object which:
 *                  recieves only one argument of the queue's element type.
 *                  returns a boolean value.
 * @return Returns a reference to the new queue.
 */
 
-template<class ElementType> 
-    Queue<ElementType>& filter(Queue<ElementType> Queue, bool *predict(ElementType));
+template<class T> 
+    Queue<T>& filter(Queue<T> Queue, bool *predict(T));
     
 
 /*
 * transform:
 * The function recieves a queue and an operation, and applies the operation on each element in the queue.
 * Notice that it is making direct changes to the queue, not making a copy!
-* @param queue - A queue of type ElementType.
+* @param queue - A queue of type T.
 * @param transform - A pointer to a function or a function object which:
 *                  recieves one argument of the queue's element type.
-*                  returns a value of ElementType.
+*                  returns a value of T.
 */
-template<class ElementType> 
-    void transform(Queue<ElementType> queue, ElementType *transform(ElementType element));
+template<class T> 
+    void transform(Queue<T> queue, T *transform(T element));
 
 /*
 * reduce:
@@ -186,15 +219,15 @@ template<class ElementType>
 * It applies the operation on the last element in the queue with the value,
 * then applies the operation on the previous element with the result, and continues recursevely.
 * When the operation has been applied on all the elements, it will return the result of the last operation.
-* @param queue - A queue of type ElementType.
+* @param queue - A queue of type T.
 * @param binaryOperator - A pointer to a function or a function object which:
 *                  recieves two argument of the queue's element type.
-*                  returns a value of ElementType.
+*                  returns a value of T.
 *                  
 * @param startingValue - a value of the queue's element type.
 */
-template<class ElementType>
-    ElementType reduce(queue<ElementType> queue, ElementType *binaryOperator(ElementType element, ElementType externalValue), ElementType startingValue);
+template<class T>
+    T reduce(queue<T> queue, T *binaryOperator(T element, T externalValue), T startingValue);
 
 
 
